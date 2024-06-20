4c2e07aef9e68f8f039c9a78e12e3db41f2177b7
AlexSerb <aserbin190@gmail.com>
2024-06-20 18:22:19 +0400

    .env file update

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,14 +4,9 @@ import os
 
 
 class Config(object):
-    DEBUG = True
-    DEVELOPMENT = True
     SECRET_KEY = settings.SECRET_KEY
 
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}"
-        + f"@{settings.DATABASE_ADDRESS}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
-    )
+    SQLALCHEMY_DATABASE_URI = settings.DATABASE_URI
 
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -4,9 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_ADDRESS = os.getenv("DATABASE_ADDRESS")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-DATABASE_PORT = os.getenv("DATABASE_PORT")
-DATABASE_USER = os.getenv("DATABASE_USER")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_URI = os.getenv("DATABASE_URI")
 SECRET_KEY = os.getenv("SECRET_KEY")
